0d9676c0ca663f36fd88edeb9733e4d8e9ebafa3
Santiago Vega F <84238334+SantiagoV323@users.noreply.github.com>
2023-12-06 20:20:55 +0000

    Copilot third commit fix

--- comments.js
+++ comments.js
@@ -1,47 +1,41 @@
 //create web server
+// 1. load modules
 var express = require('express');
-var router = express.Router();
-var path = require('path');
-var fs = require('fs');
-var bodyParser = require('body-parser');
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(bodyParser.json());
+var bodyParser = require('body-parser');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var db = require('./db');
+var util = require('./util');
+var async = require('async');
+
+// 2. set port
+var port = process.env.PORT || 3000;
+
+// 3. set middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-//create connection with mongoDB
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/CommentsDB');
-mongoose.Promise = global.Promise;
-//create schema
-var Schema = mongoose.Schema;
-var commentSchema = new Schema({
-    name: String,
-    email: String,
-    comment: String,
-    date: String
-});
-//create model
-var Comment = mongoose.model('Comment', commentSchema);
-//get all comments from database
-router.get('/all', function(req, res) {
-    Comment.find({}, function(err, data) {
-        if (err) throw err;
-        res.send(data);
-    });
-});
-//add new comment to database
-router.post('/new', urlencodedParser, function(req, res) {
-    var newComment = Comment(req.body).save(function(err, data) {
-        if (err) throw err;
-        res.send(data);
-    });
-});
-//delete comment from database
-router.post('/delete', urlencodedParser, function(req, res) {
-    Comment.find({ _id: req.body.id }).remove(function(err, data) {
-        if (err) throw err;
-        res.send(data);
-    });
-});
-//export module
-module.exports = router;
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+// 4. set router
+router.route('/comments')
+	.get(function(req, res) {
+		fs.readFile(__dirname + '/public/data/comments.json', function(err, data) {
+			if (err) {
+				res.status(500).send(err);
+				return;
+			}
+			var comments = JSON.parse(data);
+			res.json(comments);
+		});
+	})
+	.post(function(req, res) {
+		fs.readFile(__dirname + '/public/data/comments.json', function(err, data) {
+			if (err) {
+				res.status(500).send(err);
+				return;
+			}
+			var comments = JSON.parse(data);
+			var newComment = {
+				id: Date.now(),
